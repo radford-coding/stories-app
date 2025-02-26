4b90af729af704d7515563000c32c459d08bdeb8
Matt Radford <quinquex@gmail.com>
2025-02-26 11:38:18 -0800

    init README.md, parsing more data

---
+++ README.md
@@ -0,0 +1,7 @@
+## Attributions
+
+1. [Aesop's Fables API](https://github.com/poseidon-code/shortstories-api)
+2. [making JS API requests](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
+3. [longer short stories](https://americanliterature.com/home/)
+4. [custom client-side form validation](https://stackoverflow.com/a/61873952)
+5. [ChatGPT API for generating stories](https://medium.com/design-bootcamp/how-to-build-a-story-generator-using-openais-gpt-3-and-javascript-d3697e1814f2)
--- queries/queries.js
+++ queries/queries.js
@@ -1,45 +1,68 @@
 /* for one-time queries to set up our app's database */
 
-let temp;
-const getShortestStories = async () => {
-    try {
-        const response = await fetch('https://shortstories-api.onrender.com/stories');
-        if (!response.ok) {
-            throw new Error(`response status: ${response.status}`)
-        };
-        return await response.json();
-    } catch (error) {
-        console.log(error);
-    };
-};
-
-temp = await getShortestStories();
-
-temp = Object.values(temp);
-
-console.log(temp[0]);
-console.log(temp[1]);
-console.log(temp[2]);
-
-// temp.forEach(element => {
-//     console.log(element.author);
-// });
-
-// const writeShortestStories = async () => {
+// let temp;
+// const getShortestStories = async () => {
 //     try {
-//         await fetch('http://localhost:3000/', {
-//             method: 'POST',
-//             body: JSON.stringify({
-//                 name: 'test title',
-//                 body: 'test body',
-//                 owner: '67be74bc1ab0ecf6383584a0',
-//                 author: 'Aesop\'s Fables',
-//                 vibes: ['adage', 'fable'],
-//             }),
-//         });
+//         const response = await fetch('https://shortstories-api.onrender.com/stories');
+//         if (!response.ok) {
+//             throw new Error(`response status: ${response.status}`)
+//         };
+//         return await response.json();
 //     } catch (error) {
 //         console.log(error);
 //     };
 // };
 
-// writeShortestStories();
+// temp = await getShortestStories();
+
+// temp = Object.values(temp);
+
+// console.log(temp[0]);
+// console.log(temp[1]);
+// console.log(temp[2]);
+
+// temp.forEach(element => {
+//     console.log(element.author);
+// });
+
+
+// const getShortStories = async () => {
+//     try {
+//         const response = await fetch('https://americanliterature.com/100-great-short-stories/');
+//         if (!response.ok) {
+//             throw new Error(`response status: ${response.status}`)
+//         };
+//         return await response.string();
+//     } catch (error) {
+//         console.log(error);
+//     };
+// };
+
+// let temp = await getShortStories();
+
+// temp = parseFromString(temp, 'text/html');
+
+// console.log(temp);
+
+
+
+const shortStoryData = require('./short-stories.json');
+
+// console.log(Object.keys(shortStoryData));
+
+const stories1 = [shortStoryData.Fantasy, shortStoryData.Mystery, shortStoryData.Horror, shortStoryData.Philosophy].flat();
+
+const findDuplicates = (array) => {
+    return array.filter((element, index) => array.indexOf(element) !== index);
+};
+
+console.log(stories1.length);
+console.log(findDuplicates(stories1));
+console.log(stories1[0]);
+
+
+
+
+// console.log(shortStoryData);
+
+// console.log(shortStoryData.Fantasy[0].Content);
