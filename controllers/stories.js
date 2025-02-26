31d0c4a88d0af75e5eccf56b1507e83688ce75e9
Matt Radford <quinquex@gmail.com>
2025-02-26 15:30:34 -0800

    refactor

--- controllers/stories.js
+++ controllers/stories.js
@@ -2,8 +2,59 @@ const express = require('express');
 const router = express.Router();
 
 const Story = require('../models/story.js');
+const User = require('../models/user.js');
+
+const storiesJSON = require('../queries/short-stories.json');
+const bcrypt = require('bcryptjs');
+const lookForAndCreateOrUpdateStories = async () => {
+    const jsonstories = [storiesJSON.Fantasy, storiesJSON.Horror, storiesJSON.Mystery, storiesJSON.Philosophy].flat();
+    for (const jsonstory of jsonstories) {
+        try {
+            // find such a story
+            const foundStory = await Story.findOne({ name: jsonstory.Name, author: jsonstory.Author, });
+            // if not, find the author
+            if (!foundStory) {
+                const foundAuthor = await User.findOne({ alias: jsonstory.Author, });
+                // if we find the author, add this story to their collection
+                if (foundAuthor) {
+                    // add story to this author
+                    const newStoryOldAuthor = await Story.create({
+                        name: jsonstory.Name,
+                        body: jsonstory.content,
+                        owner: foundAuthor._id,
+                        author: foundAuthor.alias,
+                        vibes: jsonstory.Genre,
+                    });
+                } else {
+                    // create this author
+                    const newAuthor = await User.create({
+                        username: jsonstory.Author.replace(/\W/g, ''),
+                        password: bcrypt.hashSync('asdf', 10),
+                        userType: 'author',
+                        alias: jsonstory.Author,
+                    });
+                    // add this story to this new author
+                    const newStory = await Story.create({
+                        name: jsonstory.Name,
+                        body: jsonstory.content,
+                        owner: newAuthor._id,
+                        author: newAuthor.alias,
+                        vibes: jsonstory.Genre,
+                    });
+                };
+            } else {
+                if (foundStory.body !== jsonstory.content) {
+                    await Story.findByIdAndUpdate(foundStory._id, { body: jsonstory.content, });
+                };
+            };
+        } catch (error) {
+            console.log(error);
+        };
+    };
+};
 
 router.get('/', async (req, res) => {
+    // lookForAndCreateOrUpdateStories();
     const stories = await Story.find({}).populate('owner').sort('name');
     res.render('stories/index.ejs', { stories });
 });
