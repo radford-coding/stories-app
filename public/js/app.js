b4ce63620c205e68535757876cbf8cd8389f61ac
Matt Radford <quinquex@gmail.com>
2025-03-03 10:01:44 -0700

    custom registration validation

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,23 @@
+const usernameComplexity = () => {
+    const username = document.querySelector('input[name=username]');
+    const usernamePattern = /^\w{6,24}$/;
+    if (usernamePattern.test(username)) {
+        username.setCustomValidity('');
+    } else {
+        username.setCustomValidity('Username must be 6-24 alphanumeric characters.');
+    };
+};
+
+const pwdComplexity = () => {
+    const pwd = document.querySelector('input[name=password');
+    const pwdPattern = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/;
+    if (pwdPattern.test(pwd.value)) {
+        pwd.setCustomValidity('');
+    } else {
+        pwd.setCustomValidity('Password must be 8-32 characters, and contain a number, a lowercase letter, and an uppercase letter.');
+    };
+};
+
 const pwdVerify = () => {
     const pwd = document.querySelector('input[name=password');
     const confirm = document.querySelector('input[name=confirmPassword]');
