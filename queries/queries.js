c8daad3afbc5e93b882f686394dd6c36d897b9da
Matt Radford <quinquex@gmail.com>
2025-03-03 10:04:04 -0700

    remove console logs

--- queries/queries.js
+++ queries/queries.js
@@ -1,29 +1,29 @@
 /* for one-time queries to set up our app's database */
 
-// let temp;
-// const getShortestStories = async () => {
-//     try {
-//         const response = await fetch('https://shortstories-api.onrender.com/stories');
-//         if (!response.ok) {
-//             throw new Error(`response status: ${response.status}`)
-//         };
-//         return await response.json();
-//     } catch (error) {
-//         console.log(error);
-//     };
-// };
+let temp;
+const getShortestStories = async () => {
+    try {
+        const response = await fetch('https://shortstories-api.onrender.com/stories');
+        if (!response.ok) {
+            throw new Error(`response status: ${response.status}`)
+        };
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    };
+};
 
-// temp = await getShortestStories();
+temp = await getShortestStories();
 
-// temp = Object.values(temp);
+temp = Object.values(temp);
 
-// console.log(temp[0]);
-// console.log(temp[1]);
-// console.log(temp[2]);
+console.log(temp[0]);
+console.log(temp[1]);
+console.log(temp[2]);
 
-// temp.forEach(element => {
-//     console.log(element.author);
-// });
+temp.forEach(element => {
+    console.log(element.author);
+});
 
 
 const getShortStories = async () => {
